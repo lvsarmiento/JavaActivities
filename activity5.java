6386e8f7ef3aa2300ba981f66e5bff73d44a23be
Louise V. Sarmiento <lvsarmiento@headoffice.corp.bpi.com.ph>
2025-12-04 19:08:59 +0800

    add M1 act5

---
+++ activity5.java
@@ -0,0 +1,17 @@
+package ph.com.bpi.hello;
+
+public class activity5 {
+
+	public static void main(String[] args) {
+
+    int sum = 0;
+
+    for (int i = 1; i <= 50; i++) {
+        sum += i;
+    }
+
+    System.out.println("Sum = " + sum);
+}
+}
+//ACTIVITY 5
+//LOUISE SARMIENTO
