f336219ad8722bb2fe33becd74fbe015dcf9a9ad
Louise V. Sarmiento <lvsarmiento@headoffice.corp.bpi.com.ph>
2025-12-04 18:05:44 +0800

    add M2 act2

---
+++ seatwork.java
@@ -0,0 +1,11 @@
+package M2_Activity2;
+
+public class seatwork {
+
+    public static void main(String[] args) {
+
+        whiteboard wb = new whiteboard();
+        int sum = wb.addNumbers(5,3);
+        System.out.println("The total is: " + String.valueOf(sum) );
+    }
+}
---
+++ whiteboard.java
@@ -0,0 +1,10 @@
+package M2_Activity2;
+
+public class whiteboard {
+
+    int addNumbers(int addend1, int addend2){
+        int sum = addend1 + addend2;
+        return sum;
+    }
+    
+}
