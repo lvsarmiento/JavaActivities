cf2b6bcaef6cc54f6abec322b81e27c427fc825f
Louise V. Sarmiento <lvsarmiento@headoffice.corp.bpi.com.ph>
2025-12-04 19:15:19 +0800

    add M1 act1

---
+++ activity1.java
@@ -0,0 +1,20 @@
+package ph.com.bpi.hello;
+
+import java.util.Scanner;
+
+public class activity1 {
+
+	public static void main(String[] args) {		
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+        
+        System.out.println("Hello, " + name + "!");
+
+	}
+
+}
+//ACTIVITY 1
+//LOUISE SARMIENTO
